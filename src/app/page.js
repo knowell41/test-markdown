2eb8c837236268983adb319cc36bccd093af54d3
Emmanuel Rustia Pagayonan <emmanuel_pagayonan_28@yahoo.com>
2023-09-30 13:43:07 +0800

    remove unnecessary import

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
-import './globals.css';
 
 const getBlogProps = async () => {
   const response = await axios.get('http://127.0.0.1:8000/blog/1')
